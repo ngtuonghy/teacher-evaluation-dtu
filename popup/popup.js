288dfd35e992de28866073bdb68f53227a9f752d
ngtuonghy <ngtuonghy@gmail.com>
2024-10-13 17:03:14 +0700

    misc, add animation ative comfirm button

--- popup/popup.js
+++ popup/popup.js
@@ -1,97 +1,95 @@
 chrome.storage.sync.get(["lecturerSelect"], function (items) {
-  var selectElement = document.getElementById("lecturerSelect");
-  if (items.lecturerSelect !== undefined) {
-    selectElement.value = items.lecturerSelect;
-  } else {
-    selectElement.value = "1";
-  }
+	var selectElement = document.getElementById("lecturerSelect");
+	if (items.lecturerSelect !== undefined) {
+		selectElement.value = items.lecturerSelect;
+	} else {
+		selectElement.value = "1";
+	}
 });
 
 chrome.storage.sync.get(["skipCheckbox"], function (items) {
-  let checkboxElement = document.querySelector(".skipCheckbox");
-  if (items.skipCheckbox !== undefined) {
-    checkboxElement.checked = items.skipCheckbox;
-  } else {
-    checkboxElement.checked = true;
-  }
+	let checkboxElement = document.querySelector(".skipCheckbox");
+	if (items.skipCheckbox !== undefined) {
+		checkboxElement.checked = items.skipCheckbox;
+	} else {
+		checkboxElement.checked = true;
+	}
 });
 
 chrome.storage.sync.get(["scrollCheckbox"], function (items) {
-  let checkboxElement = document.querySelector(".scrollCheckbox");
-  if (items.scrollCheckbox !== undefined) {
-    checkboxElement.checked = items.scrollCheckbox;
-  } else {
-    checkboxElement.checked = true;
-  }
+	let checkboxElement = document.querySelector(".scrollCheckbox");
+	if (items.scrollCheckbox !== undefined) {
+		checkboxElement.checked = items.scrollCheckbox;
+	} else {
+		checkboxElement.checked = true;
+	}
 });
 
 const selectAll = (num, checkboxValue, cbScrollValue) => {
-  if (cbScrollValue === true) {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-  }
+	if (cbScrollValue === true) {
+		window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+	}
 
-  if (checkboxValue === true) {
-    var textAreas = document.querySelectorAll("textarea");
-    textAreas.forEach(function (textArea) {
-      textArea.innerHTML += " ";
-    });
-  }
+	if (checkboxValue === true) {
+		var textAreas = document.querySelectorAll("textarea");
+		textAreas.forEach(function (textArea) {
+			textArea.innerHTML += " ";
+		});
+	}
 
-  let radioButtons = document.querySelectorAll('input[type="radio"]');
-  let groupCounts = {};
+	let radioButtons = document.querySelectorAll('input[type="radio"]');
+	let groupCounts = {};
 
-  for (let i = 0; i < radioButtons.length; i++) {
-    let radioButton = radioButtons[i];
-    if (radioButton.name && radioButton.name !== "") {
-      if (
-        groupCounts[radioButton.name] !== undefined &&
-        groupCounts[radioButton.name] !== null
-      ) {
-        groupCounts[radioButton.name] = groupCounts[radioButton.name] + 1;
-      } else {
-        groupCounts[radioButton.name] = 1;
-      }
-      if (groupCounts[radioButton.name] === num) {
-        radioButton.checked = true;
-      }
-    }
-  }
+	for (let i = 0; i < radioButtons.length; i++) {
+		let radioButton = radioButtons[i];
+		if (radioButton.name && radioButton.name !== "") {
+			if (
+				groupCounts[radioButton.name] !== undefined &&
+				groupCounts[radioButton.name] !== null
+			) {
+				groupCounts[radioButton.name] = groupCounts[radioButton.name] + 1;
+			} else {
+				groupCounts[radioButton.name] = 1;
+			}
+			if (groupCounts[radioButton.name] === num) {
+				radioButton.checked = true;
+			}
+		}
+	}
 };
 
 async function getCurrentTab() {
-  try {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  } catch (error) {
-    console.error("Error occurred while getting current tab:", error);
-    return null;
-  }
+	try {
+		let queryOptions = { active: true, lastFocusedWindow: true };
+		let [tab] = await chrome.tabs.query(queryOptions);
+		return tab;
+	} catch (error) {
+		console.error("Error occurred while getting current tab:", error);
+		return null;
+	}
 }
-
 const confirm = () => {
-  let selectElement = document.getElementById("lecturerSelect");
-  let selectedValue = parseInt(selectElement.value);
+	getCurrentTab().then((tab) => {
+		if (tab == null || !tab.url.includes("https://mydtu.duytan.edu.vn/"))
+			return;
+		let selectElement = document.getElementById("lecturerSelect");
+		let selectedValue = parseInt(selectElement.value);
 
-  let checkboxElement = document.querySelector(".skipCheckbox");
-  let checkboxSkip = checkboxElement.checked;
+		let checkboxElement = document.querySelector(".skipCheckbox");
+		let checkboxSkip = checkboxElement.checked;
 
-  let cbScrollElement = document.querySelector(".scrollCheckbox");
-  let checkboxScroll = cbScrollElement.checked;
+		let cbScrollElement = document.querySelector(".scrollCheckbox");
+		let checkboxScroll = cbScrollElement.checked;
 
-  chrome.storage.sync.set({ lecturerSelect: selectedValue });
-  chrome.storage.sync.set({ skipCheckbox: checkboxSkip });
-  chrome.storage.sync.set({ scrollCheckbox: checkboxScroll });
-  // console.log(checkboxValue);
-  getCurrentTab().then((tab) => {
-    if (tab == null || !tab.url.includes("https://mydtu.duytan.edu.vn/"))
-      return;
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: selectAll,
-      args: [selectedValue, checkboxSkip, checkboxScroll],
-    });
-  });
+		chrome.storage.sync.set({ lecturerSelect: selectedValue });
+		chrome.storage.sync.set({ skipCheckbox: checkboxSkip });
+		chrome.storage.sync.set({ scrollCheckbox: checkboxScroll });
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: selectAll,
+			args: [selectedValue, checkboxSkip, checkboxScroll],
+		});
+	});
 };
 
 document.getElementById("comfirmButton").addEventListener("click", confirm);
